99ca789d5b8075d24b3532b2fa9cb673891a0047
Shani Knobel <shani.knobel@post.runi.ac.il>
2023-12-28 00:04:12 +0200

    final final update of DamkaBoard

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -6,15 +6,23 @@ public class DamkaBoard {
 		int n= Integer.parseInt(args[0]);
 		for(int i=0;i<n;i++)
 		{
-			for(int j=0;j<n;j++)
-			{
-				System.out.print("* ");
-			}
-			System.out.println();
 			if(i%2==0)
 			{
-				System.out.print(" ");
+				for(int j=0;j<n;j++)
+				{
+					System.out.print("* ");
+				}
 			}
+				
+			else
+			{
+				for(int j=0;j<n;j++)
+				{
+					System.out.print(" *");
+				}
+			}
+
+			System.out.println();	
 		}
 	}
 }
