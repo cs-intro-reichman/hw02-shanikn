02426b012f4b185b28fb27f40e038d8d3d3d04bf
Shani Knobel <shani.knobel@post.runi.ac.il>
2023-12-27 23:46:20 +0200

    adding DamkaBoard

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,6 +3,18 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
+		int n= Integer.parseInt(args[0]);
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+			if(i%2==0)
+			{
+				System.out.print(" ");
+			}
+		}
 	}
 }
