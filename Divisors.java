1f5defc3d49bc8a3fcc73027cad56dfb6625083f
Shani Knobel <shani.knobel@post.runi.ac.il>
2023-12-28 00:22:40 +0200

    uploading Divisors

--- Divisors.java
+++ Divisors.java
@@ -2,7 +2,15 @@
  *  Gets a command-line argument (int), and prints all the divisors of the given number.
  */
 public class Divisors {
-	public static void main (String[] args) {
-		//// Put your code here
+	public static void main (String[] args) 
+	{
+		int n= Integer.parseInt(args[0]);
+		for(int i=1;i<=n;i++)
+		{
+			if(n%i==0)
+			{
+				System.out.println(i);		
+			}
+		}
 	}
 }
