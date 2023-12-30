b8b9fdd1112f9b9f5f4576b7d7891346a221977b
Shani Knobel <shani.knobel@post.runi.ac.il>
2023-12-30 22:31:11 +0200

    Uploading Perfect

--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,27 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
+		int n=Integer.parseInt(args[0]);
+		int sum=1;
+		String print=n+" is a perfect number since "+n+" = 1";
+
+		for(int i=2;i<n;i++)
+		{
+			if(n%i==0)
+			{
+				print+=" + "+i;
+				sum+=i;
+			}
+		}
+
+		if(n!=sum)
+		{
+			System.out.println(n+" is not a perfect number");
+		}
+		else
+		{
+			System.out.println(print);
+		}
+	
 	}
 }
